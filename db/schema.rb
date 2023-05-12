1a35e73780121eee00ab17ea92bcc5cf5f53ae7d
Martin <souzam@sonic.net>
2023-05-12 13:51:20 -0400

    Associated cats & humans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_12_172905) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_12_174406) do
   create_table "cats", force: :cascade do |t|
     t.string "name"
     t.integer "age"
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_12_172905) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_cats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_12_172905) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cats", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,16 @@ Cat.destroy_all
 User.destroy_all
 puts "Complete"
 
-puts "Creating cats..."
-beastie = Cat.create(name: "Beastie", age: 4, color: "black", sex: "female", disposition: "fierce")
-tiger = Cat.create(name: "Tiger", age: 14, color: "tabby", sex: "male", disposition: "cuddly")
-puts "Complete"
-
 puts "Creating humans..."
 leah = User.create(name: "Leah Cardoz", username: "lcardoz", password: "password", location: "Bushwick")
 martin = User.create(name: "Martin Souza", username: "souzam", password: "password", location: "East Village")
 kayla = User.create(name: "Kayla Hunnewell", username: "kayla_h", password: "password", location: "Los Angeles")
 puts "Complete"
+
+puts "Creating cats..."
+beastie = Cat.create(name: "Beastie", age: 4, color: "black", sex: "female", disposition: "fierce", user_id: martin.id)
+tiger = Cat.create(name: "Tiger", age: 14, color: "tabby", sex: "male", disposition: "cuddly", user_id: kayla.id)
+puts "Complete"
+
 
 puts "Seeding complete"
