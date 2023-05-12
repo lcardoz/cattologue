ea751d90a62fae5119b9b4bad959982c732514f9
Martin <souzam@sonic.net>
2023-05-12 13:36:11 -0400

    Created users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,18 @@
 
 puts "Deleting old data..."
 Cat.destroy_all
+User.destroy_all
 puts "Complete"
 
 puts "Creating cats..."
 beastie = Cat.create(name: "Beastie", age: 4, color: "black", sex: "female", disposition: "fierce")
 tiger = Cat.create(name: "Tiger", age: 14, color: "tabby", sex: "male", disposition: "cuddly")
+puts "Complete"
+
+puts "Creating humans..."
+leah = User.create(name: "Leah Cardoz", username: "lcardoz", password: "password", location: "Bushwick")
+martin = User.create(name: "Martin Souza", username: "souzam", password: "password", location: "East Village")
+kayla = User.create(name: "Kayla Hunnewell", username: "kayla_h", password: "password", location: "Los Angeles")
 puts "Complete"
 
 puts "Seeding complete"
