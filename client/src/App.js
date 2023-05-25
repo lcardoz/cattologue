1bf0a85a686f8bcb8a2b22da8a0d1d22b68fd2b1
Leah Cardoz <leahcardoz@gmail.com>
2023-05-25 14:15:59 -0400

    set up react-router-dom, made components and routes for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import Cats from './components/Cats';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+import NavBar from './components/NavBar';
+import Signup from './components/Signup';
+import UserProfile from './components/UserProfile';
+import NoPage from './components/NoPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="login" element={<Login />} />
+      <Route path="cats" element={<Cats />} />
+      <Route path="user-profile" element={<UserProfile />} />
+      <Route path="*" element={<NoPage />} />
+    </Routes>
+    </>
   );
 }
 
