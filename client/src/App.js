18b1eb03fc06afa127349993bd44b489d3f1843f
Leah Cardoz <leahcardoz@gmail.com>
2023-05-25 14:30:04 -0400

    added comment for homework and next weeks agenda

--- client/src/App.js
+++ client/src/App.js
@@ -9,17 +9,24 @@ import UserProfile from './components/UserProfile';
 import NoPage from './components/NoPage';
 
 function App() {
+
+  // next week's agenda: work on auth together, set up proxy, backend routes
+
+  // HOMEWORK due next week-
+  // Martin: signup, cats, homepage, nopage
+  // Leah: login, userprofile, navbar
+
   return (
     <>
-    <NavBar />
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="signup" element={<Signup />} />
-      <Route path="login" element={<Login />} />
-      <Route path="cats" element={<Cats />} />
-      <Route path="user-profile" element={<UserProfile />} />
-      <Route path="*" element={<NoPage />} />
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+        <Route path="cats" element={<Cats />} />
+        <Route path="user-profile" element={<UserProfile />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
     </>
   );
 }
