23b03da1f22135b9e13c7a8fe36f2945a8f38e30
Leah Cardoz <leahcardoz@gmail.com>
2023-05-28 01:08:18 -0400

    built out nav bar with nav links, worked on login frontend

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,44 @@
 import React from 'react';
 
 const Login = () => {
+
+  const handleFormSubmit = (e) => { 
+    e.preventDefault();
+
+  }
+
   return (
-    <div>Login</div>
+    <div id="login-container">
+      <h1>LOGIN</h1>
+      <div id="login-form">
+        <form onSubmit={handleFormSubmit}>
+          <label> Username: 
+            <input 
+              type="text" 
+              name="username"
+              // value={Obj.username}
+              // onChange={handleChange}
+            />
+          </label>
+          <br /> 
+          <label> Password: 
+            <input 
+              type="text" 
+              name="password"
+              // value={Obj.password}
+              // onChange={handleChange}
+            />
+          </label>
+          <br /> 
+          <br /> 
+          <input   
+            id='button-submit-form'
+            type="submit" 
+            value="Submit"
+          />
+        </form>
+      </div>   
+    </div>
   )
 }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const [user, setUser] = useState(false);
+
   return (
-    <div>NavBar</div>
+    <nav id="navbar">
+      <h1 id="nav-title">CATTOLOGUE</h1>
+      {user ?
+      <> 
+        <h4 id="nav-links">
+          <Link to="/cats">CATS</Link>
+          <Link to="/user-profile">PROFILE</Link>
+        </h4>
+      </> 
+      : 
+      <>
+        <h4 id="nav-links">
+          <Link to="/login">LOGIN</Link>
+          <Link to="/signup">SIGNUP</Link>
+        </h4>
+      </>}
+    </nav>
   )
 }
 
