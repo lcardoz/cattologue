657670157328d22c8ce43e01c14aa7c4b47e9392
Martin <souzam@sonic.net>
2023-05-30 10:37:11 -0400

    Built signup form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import {useState} from 'react';
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    username: '',
+    password: '',
+    location: ''
+  })
+
+  const handleInput = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
   return (
-    <div>Signup</div>
+    <div>
+      <h2>Sign up</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={formData.name}
+          name='name'
+          onChange={handleInput}
+        />
+        <input
+          type='text'
+          value={formData.username}
+          name='username'
+          onChange={handleInput}
+        />
+        <input
+          type='text'
+          value={formData.password}
+          name='password'
+          onChange={handleInput}
+        />
+        <input
+          type='text'
+          value={formData.location}
+          name='location'
+          onChange={handleInput}
+        />
+        <input type='submit' />
+      </form>
+    </div>
   )
 }
 
