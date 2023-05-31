0a8ecdb2cde7ae68905e16f28cbb543433c18277
Martin <souzam@sonic.net>
2023-05-31 11:10:55 -0400

    Added placeholder CatCard to Cats

--- client/src/components/CatCard.js
+++ client/src/components/CatCard.js
@@ -1,11 +1,12 @@
 const CatCard = ({cat}) => {
     return (
         <div>
-            <img src={cat.image} />
+            <p>This is a cat card</p>
+            {/* <img src={cat.image} />
             <h2>{cat.name}</h2>
             <p>{cat.age + ' | ' + cat.sex}</p>
             <p>{cat.disposition}</p>
-            {cat.user ? <p>{cat.user.name}</p> : null}
+            {cat.user ? <p>{cat.user.name}</p> : null} */}
         </div>
     )
 }
