676a9a1a7d0ee3e88ff193f5ff393a4309ab93bf
Martin <souzam@sonic.net>
2023-05-31 11:02:58 -0400

    Built basic CatCard structure

--- client/src/components/CatCard.js
+++ client/src/components/CatCard.js
@@ -1,7 +1,11 @@
 const CatCard = ({cat}) => {
     return (
         <div>
-
+            <img src={cat.image} />
+            <h2>{cat.name}</h2>
+            <p>{cat.age + ' | ' + cat.sex}</p>
+            <p>{cat.disposition}</p>
+            {cat.user ? <p>{cat.user.name}</p> : null}
         </div>
     )
 }
