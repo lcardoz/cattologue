d6151c6e31a6adaafd9b6397eb6f8de747ff7c3c
Martin <souzam@sonic.net>
2023-06-13 16:25:13 -0400

    Tidied CatForm, added initial state object

--- client/src/components/CatForm.js
+++ client/src/components/CatForm.js
@@ -2,9 +2,15 @@ import React, {useState} from 'react';
 
 const CatForm = () => {
 
-  const [formData, setFormData] = useState({
+  const initialState = {
+    name: '',
+    age: '', // Will have to convert this to int on backend
+    sex: '',
+    disposition: '',
+    image: ''
+  }
 
-  });
+  const [formData, setFormData] = useState(initialState);
 
   const handleInput = (e) => {
     setFormData({
@@ -16,48 +22,55 @@ const CatForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log('submitted form', formData)
-    // reset form data
+    setFormData(initialState)
   }
 
   return (
     <>
-      CatForm
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleInput}
-          placeholder="name"
-        />
-        <input
-          type="number"
-          name="age"
-          value={formData.age}
-          onChange={handleInput}
-          placeholder="age"
-        />
-        <select name="sex" value={formData.sex} onChange={handleInput}>
-          <option value="" disabled selected>Sex</option>
-          <option value="female">Female</option>
-          <option value="male">Male</option>
-        </select>
-        <input
-          type="text"
-          name="image"
-          value={formData.image}
-          onChange={handleInput}
-          placeholder="image"
-        />
-        <input
-          type="text"
-          name="disposition"
-          value={formData.disposition}
-          onChange={handleInput}
-          placeholder="disposition"
-        />
-        <input type="submit"/>
-      </form>
+      <h2>New Cat</h2>
+      <div className='catform'>
+        <form onSubmit={handleSubmit}>
+          <label>Name: </label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInput}
+            placeholder="Enter name..."
+            />
+          <label>Age: </label>
+          <input
+            type="number"
+            name="age"
+            value={formData.age}
+            onChange={handleInput}
+            placeholder="Enter age..."
+            />
+          <label>Sex: </label>
+          <select name="sex" value={formData.sex} onChange={handleInput}>
+            <option value="" disabled selected>Select sex...</option>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+          </select>
+          <label>Disposition: </label>
+          <input
+            type="text"
+            name="disposition"
+            value={formData.disposition}
+            onChange={handleInput}
+            placeholder="Describe disposition..."
+            />
+          <label>Image: </label>
+          <input
+            type="text"
+            name="image"
+            value={formData.image}
+            onChange={handleInput}
+            placeholder="Enter image URL..."
+            />
+          <input type="submit"/>
+        </form>
+      </div>
     </>
   )
 }
