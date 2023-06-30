41ba338c024d05256465549483575fbc34061f60
Leah Cardoz <leahcardoz@gmail.com>
2023-06-30 11:32:48 -0400

    re-added button to edit profile on userprofile component

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import {useEffect} from 'react';
+import React, {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const UserProfile = ({user, checkUser}) => {
   useEffect(checkUser, [])
 
+  const navigate = useNavigate()
+
   return (
-    <div id="profile-container">
-      <div id="profile-child">
-        <h2>Name: {user?.name}</h2>
-        {/* <img src={user.image} alt="user"/> */}
-        <h3>Location: {user?.location}</h3>
-        <div>  
-          <h3>Cats:</h3>
-          {user.cats?.map(cat => <li key={cat.id}>{cat.name}</li>)}
+    <>
+      <div style={{textAlign: 'center', marginBottom: '20px'}}>
+        <button onClick={() => navigate(`/profile/edit`)}>
+          Edit Profile
+        </button>
+      </div>
+      <div id="profile-container">
+        <div id="profile-child">
+          <h2>Name: {user?.name}</h2>
+          {/* <img src={user.image} alt="user"/> */}
+          <h3>Location: {user?.location}</h3>
+          <div>  
+            <h3>Cats:</h3>
+            {user.cats?.map(cat => <li key={cat.id}>{cat.name}</li>)}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
